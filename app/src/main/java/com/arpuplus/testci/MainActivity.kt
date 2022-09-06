07a74f87d56140e47d73c05b0bd7639236a7fbb3
akhedr <akhedr@arpuplus.com>
2022-09-06 16:58:41 +0200

    test CI

--- app/src/main/java/com/arpuplus/testci/MainActivity.kt
+++ app/src/main/java/com/arpuplus/testci/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(ui.root)
+
         ui.editTextTextPersonName.addTextChangedListener {
             viewModel.email = it.toString()
 
